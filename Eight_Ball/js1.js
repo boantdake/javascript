5af445be2e96ee9c74e23da7d29d89c7621a699d
Bo Dake <boantdake@gmail.com>
2018-02-19 12:53:06 -0500

    Initial commit

---
+++ Eight_Ball/js1.js
@@ -0,0 +1,31 @@
+var userQuestion = "?";
+var randomNumber = Math.floor(Math.random() * Math.floor(8));
+
+var eightBall = '';
+switch(randomNumber){
+    case(0):
+    	eightBall = "It is certain";
+    	break;
+    case(1):
+    	eightBall = "It is certain";
+    	break;
+    case(2):
+    	eightBall = "It is certain";
+    	break;
+    case(3):
+    	eightBall = "It is certain";
+    	break;
+    case(4):
+    	eightBall = "It is certain";
+    	break;
+    case(5):
+    	eightBall = "It is certain";
+    	break;
+  	case(6):
+    	eightBall = "It is certain";
+    	break;
+    case(7):
+    	eightBall = "It is certain";
+    	break;
+                   }
+console.log(eightBall);
